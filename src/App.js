4b9efe4d45371096622cef2268ba4ea635a69c54
resulbisgin <resulbiginresul_20011@hotmail.com>
2022-11-14 00:41:29 +0300

    finished Auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
 import Home from "./pages/Home";
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/singup" element={<Singup/>}/>
-          <Route path="/account" element={<Account/>}/>
+          <Route path="/account" element={<ProtectedRoute><Account/></ProtectedRoute>}/>
       </Routes>
     </AuthContextProvider>
     </>
--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {UserAuth} from '../context/AuthContext'
 const Singup = () => {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const {user,singUp}=UserAuth()
+    const navigate=useNavigate()
+    const handleSubmit=async (e)=>{
+        e.preventDefault()
+        try{
+             await singUp(email,password)
+             navigate('/')
+        }catch(error){
+            console.log(error)
+        }
+    }
   return (
     <>
         <div className='w-full h-screen '>
@@ -10,9 +24,15 @@ const Singup = () => {
                     <div className='max-w-[450px] h-[600px] max-auto bg-black/75 text-white'>
                         <div className='max-w-[320px] mx-auto py-16'>
                             <h1 className='text-3xl font-bold'>Sing Up</h1>
-                            <form>
-                                <input type='email' placeholder='Email' autoComplete='email' />
-                                <input type='password' placeholder='Password' autoComplete='current-password'/> 
+                            <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                                <input onChange={(e)=>setEmail(e.target.value)} className='p-3 my-2 bg-gray-700 rounded ' type='email' placeholder='Email' autoComplete='email' />
+                                <input  onChange={(e)=>setPassword(e.target.value)} className='p-3 my-2 bg-gray-700 rounded ' type='password' placeholder='Password' autoComplete='current-password'/> 
+                                <button className='bg-red-600  py-3 my-6 rounded font-bold '>Sing Up</button>
+                            <div className='flex justify-between items-center text-sm text-gray-600'>
+                                <p><input className='mr-2' type='checkbox'/>Remember me</p>
+                                <p>Need Help?</p>
+                            </div>
+                            <p className='py-8'><span className='text-gray-600'>Already subscribed to Netflix? </span>  <Link to='/login'>Sing In</Link></p>
                             </form>
                         </div>
                     </div>
